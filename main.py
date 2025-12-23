7186327ebb2aacffcc8e6f876a125af2c11e8f1b
Urias-T <triumphurias@gmail.com>
2025-12-23 23:07:43 +0100

    update: built orchestrator agent and math sub-agent

--- main.py
+++ main.py
@@ -1,6 +1,40 @@
-def main():
-    print("Hello from teacher-assistant!")
+from strands import Agent
+
+from utils import bedrock_model
+
+from sub_agents import math_agent
+
+from teacher_prompt import TEACHER_SYSTEM_PROMPT
+
+import logging
+logging.basicConfig(level=logging.INFO)
+
+
+teacher_agent = Agent(
+    model=bedrock_model,
+    system_prompt=TEACHER_SYSTEM_PROMPT,
+    # callback_handler=None,
+    tools=[math_agent],
+)
 
 
 if __name__ == "__main__":
-    main()
+    while True:
+        try:
+            user_input = input("\n> ")
+            if user_input.lower() == "exit":
+                print("Goodbye!")
+                break
+
+            response = teacher_agent(user_input)
+
+            content = str(response)
+            print(content)
+
+        except KeyboardInterrupt:
+            print("\n\nExecution Interrupted. Exiting...")
+            break
+        except Exception as e:
+            logging.error(f"An error occured: {str(e)}", exc_info=True)
+            # print(f"An error occured: {str(e)}")
+            print("Please ask another question.")
---
+++ sub_agents/math_agent/__init__.py
@@ -0,0 +1,3 @@
+from .math_agent import math_agent
+
+__all__ = ["math_agent"]
